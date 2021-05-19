58dd2e77abaa3d12f1759407faab8e3e8000e987
fantastas <msgitara@gmail.com>
2021-05-19 23:28:01 +0300

    moving to navigation and styling

--- email-script.js
+++ email-script.js
@@ -1,5 +1,28 @@
 video = document.getElementById('video');
-matuoklis = document.getElementById('matuoklis');
+var morseString = "";
+
+
+var MORSE_CODE = {".-": "a", "-...":"b", "-.-.": "c", "-..": "d", ".":"e", "..-.":"f", "--.":"g", "....":"h", 
+"..":"i", ".---":"j", "-.-":"k", ".-..":"l", "--":"m", "-.":"n", "---":"o", ".--.":"p", "--.-":"q", ".-.":"r", 
+"...":"s", "-":"t", "..-":"u", "...-":"v", ".--":"w", "-..-":"x", "-.--":"y", "--..":"z", ".----":"1", "..---":"2", 
+"...--":"3", "....-":"4", ".....":"5", "-....":"6", "--...":"7", "---..":"8", "----.":"9", "-----":"0", "|":" "};
+
+var decodeMorse = function(morseCode){
+  var words = (morseCode).split('  ');
+  var letters = words.map((w) => w.split(' '));
+  var decoded = [];
+
+  for(var i = 0; i < letters.length; i++){
+    decoded[i] = [];
+    for(var x = 0; x < letters[i].length; x++){
+        if(MORSE_CODE[letters[i][x]]){
+            decoded[i].push( MORSE_CODE[letters[i][x]] );
+        }
+    }
+  }
+
+  return decoded.map(arr => arr.join('')).join(' ');
+}
 
 function sendEmail(morseString) {
 	Email.send({
@@ -9,7 +32,7 @@ function sendEmail(morseString) {
 		To: 'msgitara@gmail.com',
 		From: "mariussurvilapastas@gmail.com",
 		Subject: "Disabled help call",
-		Body: "man reikalinga pagalba",
+		Body: decodeMorse(this.morseString),
 	})
 		.then(function (message) {
 		alert("mail sent successfully")
@@ -21,6 +44,10 @@ function sendEmail(morseString) {
 		pagebutton.click();
 	}
 
+	
+
+
+var auth = false;
 Promise.all([
 	faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
 	faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
@@ -59,64 +86,38 @@ video.addEventListener('play', () => {
 		var closedRight = false;
 
 		
-		// for(let i = 38;i <= 42; i++){
-		// 	if(i == 40){
-		// 		continue;
-		// 	}
-		// 	leftEye.push(landmarkPositions[i]._x);
-		// }
-		// console.log(leftEye);
-		
-		// let leftEyeY = 0;
-		// let leftStarterY = leftEye[0]._y * 4;
-		
-		// for(let i = 0;i < leftEye.length; i++){
-		// 	leftEyeY += leftEye[i]._y;
-		// }
-		// leftEyeY /= leftStarterY; 
-		// console.log(leftEyeY);
-		
-		// if(leftEyeY < 1.017){
-		// 	closedLeft = true;
-		// 	morseString+=".";
-		// 	console.log('.');
-		// }
-		
-		////////////////////////////////////////////////
-		// for(let i = 44;i <= 48; i++){
-		// 	if(i == 46){
-		// 		continue;
-		// 	}
-		// 	rightEye.push(landmarkPositions[i]);
-		// }
-		
-		// let rightEyeY = 0;
-		// let rightStarterY = rightEye[0]._y * 4;
-
-		// for(let i = 0;i < rightEye.length; i++){
-		// 	rightEyeY += rightEye[i]._y;
-		// }
-		// rightEyeY /= rightStarterY; 
-		// console.log(rightEyeY);
-		
-		// if(rightEyeY < 1.15){
-		// 	closedRight = true;
-		// 	morseString+="-";
-		// 	console.log('-');
-
-		// }
+	
 		
 		Object.entries(expressions).forEach(([key, value]) => {
 			if(key==='happy' && value > 0.7){
-				buttonclick();
-				return;
+				console.log('.');
+				morseString+=".";
+
+			}
+			else if(key ==='surprised' && value > 0.9){
+				morseString+=" ";
+				console.log('space');
+				
 			}
 
-			else if(key ==='surprised' && value > 0.9){
-				window.location.href = 'email.html';
+			else if(key ==='angry' && value > 0.7){
+				morseString+="-";
+				console.log('-');
+				
+			}
+			else if(key ==='sad' && value > 0.3){
+			
+				// console.log(decodeMorse(morseString));
+				buttonclick();
 				
 			}
 		
+
+		
+
+
+			// 	window.location.href = 'newPage.html';
+
 			});
 		
 			
