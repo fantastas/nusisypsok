b0c5b3f1efdd076aaffb9d6105d1cf08efe39180
fantastas <msgitara@gmail.com>
2021-05-28 09:01:53 +0300

    final changes

--- email-script.js
+++ email-script.js
@@ -7,154 +7,187 @@ video = document.getElementById('video');
 backspace = document.getElementById('backspace');
 letter = document.getElementById('letter');
 var morseString = "";
-var MORSE_CODE = {".-": "a", "-...":"b", "-.-.": "c", "-..": "d", ".":"e", "..-.":"f", "--.":"g", "....":"h", 
-"..":"i", ".---":"j", "-.-":"k", ".-..":"l", "--":"m", "-.":"n", "---":"o", ".--.":"p", "--.-":"q", ".-.":"r", 
-"...":"s", "-":"t", "..-":"u", "...-":"v", ".--":"w", "-..-":"x", "-.--":"y", "--..":"z", ".----":"1", "..---":"2", 
-"...--":"3", "....-":"4", ".....":"5", "-....":"6", "--...":"7", "---..":"8", "----.":"9", "-----":"0", "|":" "};
+var MORSE_CODE = {
+	".-": "a",
+	"-...": "b",
+	"-.-.": "c",
+	"-..": "d",
+	".": "e",
+	"..-.": "f",
+	"--.": "g",
+	"....": "h",
+	"..": "i",
+	".---": "j",
+	"-.-": "k",
+	".-..": "l",
+	"--": "m",
+	"-.": "n",
+	"---": "o",
+	".--.": "p",
+	"--.-": "q",
+	".-.": "r",
+	"...": "s",
+	"-": "t",
+	"..-": "u",
+	"...-": "v",
+	".--": "w",
+	"-..-": "x",
+	"-.--": "y",
+	"--..": "z",
+	".----": "1",
+	"..---": "2",
+	"...--": "3",
+	"....-": "4",
+	".....": "5",
+	"-....": "6",
+	"--...": "7",
+	"---..": "8",
+	"----.": "9",
+	"-----": "0",
+	"|": " "
+};
 
 
 function showImage(id) {
-    var img = document.getElementById(id);
-    img.style.visibility = 'visible';
-    img.style.display = 'block';
-   
+	var img = document.getElementById(id);
+	img.style.visibility = 'visible';
+	img.style.display = 'block';
+
 }
 
-var decodeMorse = function(morseCode){
-  var words = (morseCode).split('  ');
-  var letters = words.map((w) => w.split(' '));
-  var decoded = [];
+var decodeMorse = function (morseCode) {
+	var words = (morseCode).split('  ');
+	var letters = words.map((w) => w.split(' '));
+	var decoded = [];
 
-  for(var i = 0; i < letters.length; i++){
-    decoded[i] = [];
-    for(var x = 0; x < letters[i].length; x++){
-        if(MORSE_CODE[letters[i][x]]){
-            decoded[i].push( MORSE_CODE[letters[i][x]] );
-        }
-    }
-  }
+	for (var i = 0; i < letters.length; i++) {
+		decoded[i] = [];
+		for (var x = 0; x < letters[i].length; x++) {
+			if (MORSE_CODE[letters[i][x]]) {
+				decoded[i].push(MORSE_CODE[letters[i][x]]);
+			}
+		}
+	}
 
-  return decoded.map(arr => arr.join('')).join(' ');
+	return decoded.map(arr => arr.join('')).join(' ');
 }
 
-function getEmail(){
+function getEmail() {
 	email = document.getElementById('email').value;
-	message =  document.getElementById('message').value;
-	setCookie('email',email, 30);
+	message = document.getElementById('message').value;
+	setCookie('email', email, 30);
 	setCookie('message', message, 30);
 	console.log(document.cookie);
-  }
+}
 
-  function getCookie(cname) {
+function getCookie(cname) {
 	var name = cname + "=";
 	var ca = document.cookie.split(';');
-	for(var i = 0; i < ca.length; i++) {
-	  var c = ca[i];
-	  while (c.charAt(0) == ' ') {
-		c = c.substring(1);
-	  }
-	  if (c.indexOf(name) == 0) {
-		return c.substring(name.length, c.length);
-	  }
+	for (var i = 0; i < ca.length; i++) {
+		var c = ca[i];
+		while (c.charAt(0) == ' ') {
+			c = c.substring(1);
+		}
+		if (c.indexOf(name) == 0) {
+			return c.substring(name.length, c.length);
+		}
 	}
 	return "";
-  }
+}
 
 function sendEmail(morseString) {
 	var message = decodeMorse(this.morseString);
-	message = message.substr(0, message.length -1);
+	message = message.substr(0, message.length - 1);
 	Email.send({
-		Host: "smtp.gmail.com",
-		Username: "mariussurvilapastas@gmail.com",
-		Password: "Troleibusas123",
-		To: getCookie('email'),
-		From: "mariussurvilapastas@gmail.com",
-		Subject: "Disabled help call",
-		Body: message,
-	})
+			Host: "smtp.gmail.com",
+			Username: "mariussurvilapastas@gmail.com",
+			Password: "Troleibusas123",
+			To: getCookie('email'),
+			From: "mariussurvilapastas@gmail.com",
+			Subject: "Disabled help call",
+			Body: message,
+		})
 		.then(function (message) {
-		alert("mail sent successfully")
+			alert("mail sent successfully")
 		});
-	}
+}
 
-	function buttonclick(){
-		var pagebutton= document.getElementById("selfclick");
-		pagebutton.click();
-		window.location.href = 'home.html';
+function buttonclick() {
+	var pagebutton = document.getElementById("selfclick");
+	pagebutton.click();
+	window.location.href = 'home.html';
 
 
-	}
+}
 
 Promise.all([
 	faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-	faceapi.nets.faceExpressionNet.loadFromUri('/models')]).then(startVideo);
+	faceapi.nets.faceExpressionNet.loadFromUri('/models')
+]).then(startVideo);
+console.log(getCookie('email'));
 
-function startVideo(){
-    
-	navigator.getUserMedia(
-	{video: {} },
-	stream => video.srcObject = stream,
-	err => console.error('unable to start video'))
+function startVideo() {
+
+	navigator.getUserMedia({
+			video: {}
+		},
+		stream => video.srcObject = stream,
+		err => console.error('unable to start video'))
 }
 let i = 0;
 
 video.addEventListener('play', () => {
-   
+
 	setInterval(async () => {
-		const detections = await faceapi.detectAllFaces(video, 
-		new faceapi.TinyFaceDetectorOptions()).withFaceExpressions()
+		const detections = await faceapi.detectAllFaces(video,
+			new faceapi.TinyFaceDetectorOptions()).withFaceExpressions()
 		var expressions = detections[0].expressions;
-		
+
 		dot.style.visibility = 'hidden';
 		bruksnys.style.visibility = 'hidden';
 		space.style.visibility = 'hidden';
 		backspace.style.visibility = 'hidden';
 		letter.style.visibility = 'hidden';
 
-		
+
 		Object.entries(expressions).forEach(([key, value]) => {
-               
-                if(key==='happy' && value > 0.7){
-                    showImage('dot');
-                    morseString+=".";
-                    morzesKodas.value = morseString;
-					matuoklis.value = decodeMorse(morseString);
-					
-				}
-                else if(key ==='surprised' && value > 0.9){
-                    morseString+=" ";
-                    showImage('space');
-                 	morzesKodas.value = morseString;
-					matuoklis.value = decodeMorse(morseString);
-					
 
-				}
-				else if(key ==='angry' && value > 0.6){
-                    showImage('bruksnys');
-                    morseString+="-";
-                    morzesKodas.value = morseString;
-					matuoklis.value = decodeMorse(morseString);
-					
+			if (key === 'happy' && value > 0.7) {
+				showImage('dot');
+				morseString += ".";
+				morzesKodas.value = morseString;
+				matuoklis.value = decodeMorse(morseString);
 
-				}
-				else if(key ==='sad' && value > 0.6){
-					showImage('backspace');
-					
-					var index = morseString.split(" ");
-					
-					index = index.slice(0, index.length-1);
-					morseString = index.join(" ");
-					matuoklis.value = decodeMorse(morseString);
-					morzesKodas.value = morseString;
-                }
-				
-				else if(matuoklis.value.substr(matuoklis.value.length - 1) == 'w'){
-						morseString.substr(0,morseString.length -1);
-						showImage('letter');
-						buttonclick();
+			} else if (key === 'surprised' && value > 0.9) {
+				morseString += " ";
+				showImage('space');
+				morzesKodas.value = morseString;
+				matuoklis.value = decodeMorse(morseString);
 
-				}
-		
-		    });
-		}, 1000)
+
+			} else if (key === 'angry' && value > 0.6) {
+				showImage('bruksnys');
+				morseString += "-";
+				morzesKodas.value = morseString;
+				matuoklis.value = decodeMorse(morseString);
+
+
+			} else if (key === 'sad' && value > 0.6) {
+				showImage('backspace');
+
+				var index = morseString.split(" ");
+
+				index = index.slice(0, index.length - 1);
+				morseString = index.join(" ");
+				matuoklis.value = decodeMorse(morseString);
+				morzesKodas.value = morseString;
+			} else if (matuoklis.value.substr(matuoklis.value.length - 1) == 'w') {
+				morseString.substr(0, morseString.length - 1);
+				showImage('letter');
+				buttonclick();
+
+			}
+
+		});
+	}, 1000)
 });
--- script.js
+++ script.js
@@ -2,48 +2,48 @@ video = document.getElementById('video');
 matuoklis = document.getElementById('matuoklis');
 
 
-function changeDiv(id){
+function changeDiv(id) {
 	tevas = document.getElementById('tevas');
 	var c = tevas.children;
-	div =  document.getElementById(id);
-	for(let i = 0; i< c.length; i++){
+	div = document.getElementById(id);
+	for (let i = 0; i < c.length; i++) {
 		c[i].style.visbility = "hidden";
 		c[i].style.display = 'none';
-	 }
-	
+	}
+
 	div.style.visbility = "visible";
 	div.style.display = 'flex';
 }
 
 function setCookie(cname, cvalue, exdays) {
 	var d = new Date();
-	d.setTime(d.getTime() + (exdays*24*60*60*1000));
-	var expires = "expires="+ d.toUTCString();
+	d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+	var expires = "expires=" + d.toUTCString();
 	document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-  }
+}
 
 
-  function getEmail(){
+function getEmail() {
 	email = document.getElementById('email').value;
-	message =  document.getElementById('message').value;
-	setCookie('email',email, 30);
+	message = document.getElementById('message').value;
+	setCookie('email', email, 30);
 	setCookie('message', message, 30);
-  }
+}
 
-  function getCookie(cname) {
+function getCookie(cname) {
 	var name = cname + "=";
 	var ca = document.cookie.split(';');
-	for(var i = 0; i < ca.length; i++) {
-	  var c = ca[i];
-	  while (c.charAt(0) == ' ') {
-		c = c.substring(1);
-	  }
-	  if (c.indexOf(name) == 0) {
-		return c.substring(name.length, c.length);
-	  }
+	for (var i = 0; i < ca.length; i++) {
+		var c = ca[i];
+		while (c.charAt(0) == ' ') {
+			c = c.substring(1);
+		}
+		if (c.indexOf(name) == 0) {
+			return c.substring(name.length, c.length);
+		}
 	}
 	return "";
-  }
+}
 
 
 
@@ -51,134 +51,135 @@ function setCookie(cname, cvalue, exdays) {
 
 function sendEmail(morseString) {
 	Email.send({
-		Host: "smtp.gmail.com",
-		Username: "mariussurvilapastas@gmail.com",
-		Password: "Troleibusas123",
-		To: getCookie('email'),
-		From: "mariussurvilapastas@gmail.com",
-		Subject: "Disabled help call",
-		Body: getCookie('message'),
-	})
+			Host: "smtp.gmail.com",
+			Username: "mariussurvilapastas@gmail.com",
+			Password: "Troleibusas123",
+			To: getCookie('email'),
+			From: "mariussurvilapastas@gmail.com",
+			Subject: "Disabled help call",
+			Body: getCookie('message'),
+		})
 		.then(function (message) {
-		alert("mail sent successfully")
+			alert("mail sent successfully")
 		});
-	}
+}
 
-	function buttonclick(){
-		var pagebutton= document.getElementById("selfclick");
-		pagebutton.click();
-		window.location.href = 'home.html';
+function buttonclick() {
+	var pagebutton = document.getElementById("selfclick");
+	pagebutton.click();
+	window.location.href = 'home.html';
 
-	}
+}
 
 Promise.all([
 	faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
 	faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
 	faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-	faceapi.nets.faceExpressionNet.loadFromUri('/models')]).then(startVideo);
+	faceapi.nets.faceExpressionNet.loadFromUri('/models')
+]).then(startVideo);
 
-function startVideo(){
-	navigator.getUserMedia(
-	{video: {} },
-	stream => video.srcObject = stream,
-	err => console.error(err))
+function startVideo() {
+	navigator.getUserMedia({
+			video: {}
+		},
+		stream => video.srcObject = stream,
+		err => console.error(err))
 }
 let i = 0;
 let happiness = 0;
 video.addEventListener('play', () => {
 	setInterval(async () => {
-		const detections = await faceapi.detectAllFaces(video, 
-		new faceapi.TinyFaceDetectorOptions()).
+		const detections = await faceapi.detectAllFaces(video,
+			new faceapi.TinyFaceDetectorOptions()).
 		withFaceLandmarks().withFaceExpressions()
 		var expressions = detections[0].expressions;
-		
 
-		
-		
+
+
+
 		Object.entries(expressions).forEach(([key, value]) => {
-			if(key==='happy'){
-				matuoklis.value = parseInt(value*100);
+			if (key === 'happy') {
+				matuoklis.value = parseInt(value * 100);
 			}
-			if(key==='happy' && value > 0.99){
+			if (key === 'happy' && value > 0.99) {
 				buttonclick();
 				return;
+			} else if (key === 'surprised' && value > 0.99) {
+				window.location.href = 'email.html';
+
 			}
 
-			else if(key ==='surprised' && value > 0.99){
-				window.location.href = 'email.html';
-				
-			}
-		
-			});
-		
-			
-			
+		});
+
+
+
 	}, 1000)
 });
 
 
-window.addEventListener("load", function(){
-	setTimeout(function(){ 	
+window.addEventListener("load", function () {
+	setTimeout(function () {
 		overlay = document.getElementById('overlay');
-		overlay.style.display = "none"; }, 2000);
+		overlay.style.display = "none";
+	}, 2000);
 
-	
-	});
+
+});
 
 
 
 
 
 // const landmarkPositions = detections[0].landmarks._positions;
-		// var leftEye = [];
-		// var rightEye = [];
-		// var closedLeft = false;
-		// var closedRight = false;
+// var leftEye = [];
+// var rightEye = [];
+// var closedLeft = false;
+// var closedRight = false;
 
-		
-		// for(let i = 38;i <= 42; i++){
-		// 	if(i == 40){
-		// 		continue;
-		// 	}
-		// 	leftEye.push(landmarkPositions[i]._x);
-		// }
-		// console.log(leftEye);
-		
-		// let leftEyeY = 0;
-		// let leftStarterY = leftEye[0]._y * 4;
-		
-		// for(let i = 0;i < leftEye.length; i++){
-		// 	leftEyeY += leftEye[i]._y;
-		// }
-		// leftEyeY /= leftStarterY; 
-		// console.log(leftEyeY);
-		
-		// if(leftEyeY < 1.017){
-		// 	closedLeft = true;
-		// 	morseString+=".";
-		// 	console.log('.');
-		// }
-		
-		////////////////////////////////////////////////
-		// for(let i = 44;i <= 48; i++){
-		// 	if(i == 46){
-		// 		continue;
-		// 	}
-		// 	rightEye.push(landmarkPositions[i]);
-		// }
-		
-		// let rightEyeY = 0;
-		// let rightStarterY = rightEye[0]._y * 4;
 
-		// for(let i = 0;i < rightEye.length; i++){
-		// 	rightEyeY += rightEye[i]._y;
-		// }
-		// rightEyeY /= rightStarterY; 
-		// console.log(rightEyeY);
-		
-		// if(rightEyeY < 1.15){
-		// 	closedRight = true;
-		// 	morseString+="-";
-		// 	console.log('-');
+// for(let i = 38;i <= 42; i++){
+// 	if(i == 40){
+// 		continue;
+// 	}
+// 	leftEye.push(landmarkPositions[i]._x);
+// }
+// console.log(leftEye);
 
-		// }
+// let leftEyeY = 0;
+// let leftStarterY = leftEye[0]._y * 4;
+
+// for(let i = 0;i < leftEye.length; i++){
+// 	leftEyeY += leftEye[i]._y;
+// }
+// leftEyeY /= leftStarterY; 
+// console.log(leftEyeY);
+
+// if(leftEyeY < 1.017){
+// 	closedLeft = true;
+// 	morseString+=".";
+// 	console.log('.');
+// }
+
+////////////////////////////////////////////////
+// for(let i = 44;i <= 48; i++){
+// 	if(i == 46){
+// 		continue;
+// 	}
+// 	rightEye.push(landmarkPositions[i]);
+// }
+
+// let rightEyeY = 0;
+// let rightStarterY = rightEye[0]._y * 4;
+
+// for(let i = 0;i < rightEye.length; i++){
+// 	rightEyeY += rightEye[i]._y;
+// }
+// rightEyeY /= rightStarterY; 
+// console.log(rightEyeY);
+
+// if(rightEyeY < 1.15){
+// 	closedRight = true;
+// 	morseString+="-";
+// 	console.log('-');
+
+// }
